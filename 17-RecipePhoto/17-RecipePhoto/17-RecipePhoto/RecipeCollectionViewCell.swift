19829800cc14a31a6f5fa43a6e00dcc397033469
Melody <lengningshang@126.com>
2015-11-24 21:02:23 +0800

    collection demo

---
+++ 17-RecipePhoto/17-RecipePhoto/17-RecipePhoto/RecipeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.swift
+//  17-RecipePhoto
+//
+//  Created by Yiqi Wang on 15/11/24.
+//  Copyright © 2015年 Yiqi Wang. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ 17-RecipePhoto/17-RecipePhoto/17-RecipePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17-RecipePhoto
+//
+//  Created by Yiqi Wang on 15/11/24.
+//  Copyright © 2015年 Yiqi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
